328ae9b75eae2e2a5d54c4bb4897b72a6f9b7977
attraverso <annachiara.traverso@gmail.com>
2020-06-18 12:03:33 +0200

    [REUPLOAD] file directory adjustments

--- versione-ajax/index.php
+++ versione-ajax/index.php
@@ -6,7 +6,7 @@
   <link rel="stylesheet" href="../dist/style.css">
   <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
   <script src="https://cdn.jsdelivr.net/npm/handlebars@latest/dist/handlebars.js"></script>
-  <script src="../dist/main.js" charset="utf-8"></script>
+  <script src="../main.js" charset="utf-8"></script>
 </head>
 <body>
   <header></header>
